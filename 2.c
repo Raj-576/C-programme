b3b3ddde24fc7529fdbf0dec481a56dde4466d59
Ankit Raj <147795844+lucifer5094@users.noreply.github.com>
2024-03-13 04:31:19 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,36 +1,53 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     float num1;
     float num2;
     char op;
-    float result ;
-    printf("Enter the first number :");
-    scanf("%f",&num1);
-    printf("Enter the operation :");
-    scanf(" %c",&op);
-    printf("Enter the second number :");
-    scanf("%f",&num2);
+    float result;
+
+    printf("Enter the first number: ");
+    if (scanf("%f", &num1) != 1) {
+        printf("Invalid input for the first number\n");
+        return 1;
+    }
+
+    printf("Enter the operation (+, -, *, /): ");
+    scanf(" %c", &op);
+    getchar(); 
+
+    printf("Enter the second number: ");
+    if (scanf("%f", &num2) != 1) {
+        printf("Invalid input for the second number\n");
+        return 1;
+    }
+
     switch (op)
     {
-        case'+':
-        result = num1+num2;
-        printf("%f",result);
-        break;
-         case'-':
-        result = num1-num2;
-        printf("%f",result);
-        break;
-         case'*':
-        result = num1*num2;
-        printf("%f",result);
-        break;
-         case'/':
-        result = num1/num2;
-        printf("%f",result);
-        break;
-        default :
-        printf("the operaton is not valid :");
-        return 0;
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Error: Division by zero!\n");
+                return 1; 
+            }
+            break;
+        default:
+            printf("Invalid operation\n");
+            return 1; 
     }
+
+    printf("Result: %f\n", result);
+
+    return 0;
 }
